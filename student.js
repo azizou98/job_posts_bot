65965a41cb2fafd3da8f40397cd48827ff4b903d
azizou98 <abdelaziz.aitmoussa@etu.usthb.dz>
2023-04-03 16:18:37 +0200

    job new change

--- student.js
+++
@@ -1,6 +0,0 @@
-var calculateFullAge = require('full-age-calculator');
-
-var getfullAge = calculateFullAge.getFullAge('2020-01-01').months
-    + (calculateFullAge.getFullAge('2020-01-01').years) * 12;
-// In yyyy-mm-dd format. Example: 1999-12-25
-console.log(getfullAge + " month and\t" + calculateFullAge.getFullAge('2020-01-01').days + " days");
